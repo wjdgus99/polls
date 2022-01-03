9f54810c44247d5b8f42521b6ba1303f970623e5
pig <21800568@handong.edu>
2022-01-03 14:34:43 +0900

    Initial commit

---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+    path('/', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/5/results', views.results, name='results'),
+    path('<int:question_id>/5/vote', views.vote, name='vote'),
+]
